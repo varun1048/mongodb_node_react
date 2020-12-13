e715d9a8af8cd007fdd740177b9f667472b863cb
varun <59528216+varun1048@users.noreply.github.com>
2020-12-13 18:03:19 +0530

    done

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require("body-parser")
+const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+mongoose.connect("mongodb+srv://varundb:varun@cluster0.pqsyb.mongodb.net/notesDB",{ useNewUrlParser: true },{ useUnifiedTopology: true}, )
+
+const notesSchema   ={
+    title: String,
+    content: String
+}
+const Note = mongoose.model("Note2",notesSchema)
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+app.post('/',(req,res)=>{
+    let newNote = new Note({
+        title:req.body.title,
+        content:req.body.content
+    })
+    newNote.save()
+    res.redirect('/')
+
+})
+
+
+app.listen(3001,()=>console.log("server under :3001"))
